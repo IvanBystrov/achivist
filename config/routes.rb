9c85683b0a2966c1798e24a12b89df199179885f
Ivan Bystrov <infernokernel@gmail.com>
2016-01-17 20:03:04 +0300

    create file record controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'file_records/index'
+
+  get 'file_records/new'
+
+  get 'file_records/create'
+
+  get 'file_records/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
