e72f6aa0de5136c92ac879e9cb223bbfe7477601
Ivan Bystrov <infernokernel@gmail.com>
2016-01-17 21:42:12 +0300

    define actions for file records

--- app/controllers/file_records_controller.rb
+++ app/controllers/file_records_controller.rb
@@ -1,13 +1,28 @@
 class FileRecordsController < ApplicationController
   def index
+    @file_records = FileRecord.all
   end
 
   def new
+    @file_record = FileRecord.new
   end
 
   def create
+    @file_record = FileRecord.new(:file_record_params)
+    @file_record.save
+    #redirect_to file_records_path, notice "File has been uploaded."
+
   end
 
   def destroy
+    @file_record = FileRecord.find(params[:id])
+    @file_record.destroy
+    #redirect_to file_records_path, notice "File has been deleted."
   end
+
+private
+  def file_record_params
+    params.require(:file_record).permit(:description, :attachment)
+  end  
+
 end
